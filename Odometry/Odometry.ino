4df038514ce437fa3f2683b8262f42c3270abf0f
asgaines <andrew.s.gaines@gmail.com>
2016-05-12 11:30:12 -0600

    Improvements to calculations. Still not complete though

--- Odometry/Odometry.ino
+++ Odometry/Odometry.ino
@@ -15,16 +15,15 @@
 int startOfLoop; // Time since start of program, calculated at each loop beginning
 const float pi = 3.1415926;
 
-const float speedMotor = 0.3 / (10773 / 1000); // m / s
+const float speedMotor = 0.3 / (10773.0 / 1000.0); // m / s
 const float lengthAxle = 0.084; // meters
 
-float xPos = 0;
-float yPos = 0;
+float posX = 0;
+float posY = 0;
 float theta = pi; // Initially facing left (180 degrees)
 
-// If not moving forward, moving backwards instead
-boolean rightForward = true;
-boolean leftForward = true;
+boolean turningRight;
+boolean turningLeft;
 
 int threshold = 700;
 
@@ -43,57 +42,71 @@ void loop() {
   lineCenter = sparki.lineCenter(); // measure the center IR sensor
   lineRight  = sparki.lineRight();  // measure the right IR sensor
 
-//  if ( lineCenter < threshold ) // if line is below left line sensor
-//  {  
-//    sparki.moveForward(); // move forward
-//    rightForward = true;
-//    leftForward = true;
-//  }
-//  else{
-//    if ( lineLeft < threshold ) // if line is below left line sensor
-//    {  
-//      sparki.moveLeft(); // turn left
-//      leftForward = false;
-//      rightForward = true;
-//    }
-//  
-//    if ( lineRight < threshold ) // if line is below right line sensor
-//    {  
-//      sparki.moveRight(); // turn right
-//      leftForward = true;
-//      rightForward = false;
-//    }
-//  }
+  if ( lineCenter < threshold ) // if line is below left line sensor
+  {  
+    sparki.moveForward(); // move forward
+    turningRight = false;
+    turningLeft = false;
+  }
+  else{
+    if ( lineLeft < threshold ) // if line is below left line sensor
+    {  
+      sparki.moveLeft(); // turn left
+      turningLeft = true;
+      turningRight = false;
+    }
   
-  sparki.moveLeft();
-  leftForward = true;
-  rightForward = false;
+    if ( lineRight < threshold ) // if line is below right line sensor
+    {  
+      sparki.moveRight(); // turn right
+      turningRight = true;
+      turningLeft = false;
+    }
+  }
 
   sparki.clearLCD(); // wipe the screen
   
   sparki.print("X-position: "); // show left line sensor on screen
-  sparki.println(xPos);
+  sparki.println(posX);
   
   sparki.print("Y-position: "); // show center line sensor on screen
-  sparki.println(yPos);
+  sparki.println(posY);
   
   sparki.print("Theta: "); // show right line sensor on screen
-  sparki.println(theta);
+  sparki.println(theta/3.1415*180);
   
   sparki.updateLCD(); // display all of the information written to the screen
 
-  while (millis() - startOfLoop <= 100){
-    // Wait and do nothing, this is used to ensure Sparki was moving for 100 msec
-  }
+
+  // Stop Sparki for the calculations
+//  sparki.moveStop();
 
   // Calculate the kinematics of Sparki
   
   // Calculate change in theta
-  if (leftForward && !rightForward) {
-    theta -= speedMotor / lengthAxle;// % (2 * pi);
-  } else if (rightForward && !leftForward) {
-    theta += speedMotor / lengthAxle;// % (2 * pi);
+  if (turningRight && !turningLeft) {
+    // Sparki is turning right
+    theta -= 2.0 * (speedMotor * 0.1) / lengthAxle;
+  } else if (turningLeft && !turningRight) {
+    // Sparki is turning left
+    theta += 2.0 * (speedMotor * 0.1) / lengthAxle;
   } // No change if moving straight forward
-  
+
+  // Keep theta between 0 and 2*pi
+//  if (theta < 0) {
+//    theta += 2 * pi;
+//  } else if (theta > 2 * pi) {
+//    theta -= 2 * pi;
+//  }
+
+  // Calculate change in position
+  if (!turningLeft && !turningRight) {
+    posX += cos(theta) * (speedMotor * 0.1);
+    posY += sin(theta) * (speedMotor * 0.1);
+  }
    
+  while (millis() - startOfLoop <= 100){
+    // Wait and do nothing, this is used to ensure Sparki was moving for 100 msec
+  }
+
 }
